d5f06d9630a2564986adaf105d2922cfead4a062
kphua <kathyphua92@gmail.com>
2014-03-06 17:08:36 +0800

    Add Eggs
    Added egg model, script (collectibles.cs, GUIcounter.js)

---
+++ Dino Reloaded/Assets/Scripts/GUICounter.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var Counter : int = 0;
+
+function Start () {
+	
+}
+
+function Update () {
+	guiText.text = "Egg Count: "+Counter;
+	Debug.Log("update " + Counter);
+}
+
+function ApplyDamage () {
+	Counter++;
+	Debug.Log("called applyDamage " + Counter);
+}
