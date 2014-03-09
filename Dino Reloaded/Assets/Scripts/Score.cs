ebbcda1beac3d61bf298e81f6d2859e8eb86b478
linxea <linxea@hotmail.com>
2014-03-09 14:40:11 +0800

    updated score script

--- Dino Reloaded/Assets/Scripts/Score.cs
+++ Dino Reloaded/Assets/Scripts/Score.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class Score : MonoBehaviour
 {
 	public int score = 0;					// The player's score.
-	private int previousScore = 0;			// The score in the previous frame.
-	
+
 	void Update ()
 	{
 		// Set the score text.
