67eb7fc1af1d0d071f68b93bc929fd4e9fed03b7
linxea <linxea@hotmail.com>
2014-03-09 12:37:15 +0800

    added font- BradBunr

---
+++ Dino Reloaded/Assets/Scripts/Score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+	public int score = 0;					// The player's score.
+	private int previousScore = 0;			// The score in the previous frame.
+	
+	void Update ()
+	{
+		// Set the score text.
+		guiText.text = "Score: " + score;
+	}
+
+}
