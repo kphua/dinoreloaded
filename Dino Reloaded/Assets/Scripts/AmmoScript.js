9b697d82297e70a99b3bec8315f2feeb8a95254d
rszeph <rs_neo@hotmail.com>
2014-03-10 16:06:49 +0800

    Added Ammo, AmmonGun,AmmoScripts scripts, deleted Score.cs

---
+++ Dino Reloaded/Assets/Scripts/AmmoScript.js
@@ -0,0 +1,16 @@
+﻿
+var AmmoSpeed = 5;
+
+function start() {
+
+}
+
+function Update () {
+	transform.Translate(Vector3.forward * Time.deltaTime * AmmoSpeed);
+	//Destroy(instance.gameObject, 5);
+	//Destroy(clone.gameObject, 2);
+}
+
+function OnCollisionEnter(){
+		Destroy(gameObject);
+}
