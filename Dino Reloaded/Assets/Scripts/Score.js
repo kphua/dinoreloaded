8d7fd5443d5ed6d27309d510f14ac1bd464c94b2
linxea <linxea@hotmail.com>
2014-03-10 11:40:09 +0800

    added Score.js

---
+++ Dino Reloaded/Assets/Scripts/Score.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var score : int = 0;
+
+function Start () {
+	
+}
+
+function Update () {
+	guiText.text = "Score: " + score;
+}
+
+function IncreaseScore () {
+	score += 100;
+}
+
+function DecreaseScore () {
+	score -= 100;
+}
